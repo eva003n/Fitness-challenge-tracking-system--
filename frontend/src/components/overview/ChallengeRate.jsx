876777a0a5e2f6af5b34f0148b650a343cec2bec
Evan ngugi wambui <evanngugi547@gmail.com>
2025-04-01 20:42:30 +0300

    add change

--- frontend/src/components/overview/ChallengeRate.jsx
+++ frontend/src/components/overview/ChallengeRate.jsx
@@ -51,7 +51,7 @@ const ChallengeRate = () => {
       </p>
 
       <ResponsiveContainer width="100%" height={220}>
-        <BarChart data={CHALLENGEcOMPLETIONRATE || data && data}>
+        <BarChart data={data && data || CHALLENGEcOMPLETIONRATE}>
           <XAxis dataKey="title" />
           <YAxis />
           <Tooltip />
